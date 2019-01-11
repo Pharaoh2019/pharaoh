de606fc33eca187aacbdfeec2cf005948bf5b075
Moamen Gasser <46604562+Pharaoh2019@users.noreply.github.com>
2019-01-11 12:18:29 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,4 +10,19 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-
+        client.on('message', message => { //ping
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith(prefix + 'ping')) {
+                            if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('#5016f3')
+                        .addField('**Time Taken:**',msg + " ms :signal_strength: ")
+                        .addField('**WebSocket:**',api + " ms :signal_strength: ")
+                        .setTimestamp()
+        message.channel.send({embed:embed});
+                        }
+                    });
